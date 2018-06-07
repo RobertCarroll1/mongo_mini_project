6384873efbd0fe38b67781fb8d2936f1764488fd
Robert Carroll <robertcarrollonline@gmail.com>
2018-06-07 15:48:39 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World ...gain"
+    
+if __name__ -- "__main__":
+    app.run(host=os.environ.get("IP"))
