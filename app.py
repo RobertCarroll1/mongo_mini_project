223f262e99b059ba8a50c41deb462f8fa1b6f3bd
Robert Carroll <robertcarrollonline@gmail.com>
2018-06-08 16:00:15 +0000

    ready to style, linked to materialize library

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from flask_pymongo import pymongo
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = "task_mannager"
+app.config["MONGO_URI"] = "mongodb://admin:thefez12@ds147420.mlab.com:47420/task_mannager"
+
+mongo = PyMongo(app)
 
 @app.route("/")
-def hello():
-    return "Hello World ...again"
+@app.route("/get_tasks")
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+# @app.route("/")
+# def hello():
+#     return "Hello World ...again"
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
